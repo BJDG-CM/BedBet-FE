d4332ad02239b1918cc7867ba00bc878294afab5
6QB6 <qbqb4811@gm.gist.ac.kr>
2025-11-09 05:08:35 +0900

    fix tab icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,29 +1,56 @@
 import { Tabs } from "expo-router";
-import { Pressable, Text } from "react-native";
+import { Image } from "react-native";
 import { useAuth } from "../_layout";
 
 export default function TabsLayout() {
   const { logout } = useAuth();
+
   return (
     <Tabs
-      screenOptions={{
+      screenOptions={({ route }) => ({
+        // headerShown: false,
         headerTitleAlign: "center",
-        tabBarActiveTintColor: "black",
-      }}
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 84, // ✅ 탭바 높이 증가
+          paddingTop: 16,
+          paddingBottom: 16,
+          backgroundColor: "white",
+          borderTopWidth: 2,
+          borderTopColor: "#FEB573",
+          elevation: 10, // Android 그림자
+        },
+        tabBarIcon: ({ focused }) => {
+          let iconSource;
+
+          if (route.name === "index") {
+            iconSource = focused
+              ? require("../../assets/icons/home_active.png")
+              : require("../../assets/icons/home.png");
+          }
+          if (route.name === "cash") {
+            iconSource = focused
+              ? require("../../assets/icons/cash_active.png")
+              : require("../../assets/icons/cash.png");
+          }
+          if (route.name === "profile") {
+            iconSource = focused
+              ? require("../../assets/icons/profile_active.png")
+              : require("../../assets/icons/profile.png");
+          }
+
+          return (
+            <Image
+              source={iconSource}
+              style={{ width: 48, height: 48, resizeMode: "contain" }}
+            />
+          );
+        },
+      })}
     >
       <Tabs.Screen name="index" options={{ title: "홈" }} />
       <Tabs.Screen name="cash" options={{ title: "캐쉬" }} />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "프로필",
-          headerRight: () => (
-            <Pressable onPress={logout} style={{ paddingRight: 12 }}>
-              <Text>로그아웃</Text>
-            </Pressable>
-          ),
-        }}
-      />
+      <Tabs.Screen name="profile" options={{ title: "프로필" }} />
     </Tabs>
   );
 }
